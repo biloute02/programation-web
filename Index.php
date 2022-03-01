6ff1b2640a418ebd98e3b7edd6f7e40b06ab631e
NegaSteel <75908566+NegaSteel@users.noreply.github.com>
2022-03-01 11:13:23 +0100

    Add files via upload
    Les premiers fichiers less gooooooo
    #AbortTonClement

---
+++ Index.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Index</title>
+</head>
+<body>
+	<h1 style="text-align:center;">
+	 Share My House 
+	</h1>
+
+</body>
+</html>
+
+<!-- Faire index avec connexion ou register
+	Une fois connecté on voit les annonces,
+	en haut à gauche, un menu déroulant
+	permettant d'ajouter des amis, de se déconnecter
+	d'acceder à son profil, des annonces etc
+/!-->
