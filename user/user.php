27e351e2d56df66b6c5f04b9391dff827c332dd0
biloute02 <d.blot@etud.u-picardie.fr>
2022-03-21 10:26:07 +0100

    écriture requête pour récupérer U_ID avec pseudo user.php

--- user/user.php
+++ user/user.php
@@ -2,8 +2,11 @@
 	session_start();
 	include('../include/connex.inc.php');
 	$idcom = connex("myparam");
-    if (!empty($_SESSION['U_ID']))
+    if (!empty($_SESSION['U_ID'])) {
         $U_ID = $_SESSION['U_ID'];
+		//on protÃ¨ge U_ID qui va Ãªtre utilisÃ© dans des requÃªtes
+		$U_ID = mysqli_real_escape_string($idcom, $U_ID);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,13 +25,26 @@
         <label>Recherche :
             <input name="pseudo">
         </label>
+		<button type="submit">OK</button>
     </form>
 <?php
+	// si le pseudo est renseignÃÂ©,
     if (!empty($_POST['pseudo'])) {
-        //vérifier que l'utilisateur existe
-    } 
-    $pseudo = mysqli_real_escape_string($pseudo);
-    $result = mysqli_query($idcom, 'SELECT * from Utilisateur WHERE ');
+    	//verifier que l'utilisateur existe
+		//
+		$pseudo = mysqli_real_escape_string($idcom, $_POST['pseudo']);
+		$result = mysqli_query($idcom, "SELECT U_ID from Utilisateur WHERE pseudo = '$pseudo'");
+		if (mysqli_num_rows($result) > 2) {
+			echo "<p>Plusieurs résultats possibles</p>";
+		}
+		print_r($result);
+	}
+	if (!empty($U_ID)) {
+
+	} else {
+		echo "<p>Ã©Vous n'ÃÂªtes pas connectÃÂ©</p>";
+		echo "<p>Faites une recherche d'utilisateur</p>";
+	}
 ?>
 </body>
 </html>
