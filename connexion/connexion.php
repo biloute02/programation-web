7058fccf367a6b3765566db663f919b3968e018b
NegaSteel <boudoux.lilian@gmail.com>
2022-03-22 08:55:43 +0100

    Ajout du U_ID et du pseudo

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -3,6 +3,10 @@ include "param_mysql.php";
 session_start();
 
 if (isset($_POST['connexion'])) {
+	define("pseudo", "");
+
+	error_reporting (E_ALL ^ E_NOTICE);
+
 	$email = htmlentities($_POST['email'], ENT_QUOTES, "UTF-8"); // ENT_QUOTES -> pour les guillemets simple et double
 	$password = htmlentities($_POST['password'], ENT_QUOTES, "UTF-8");
 	if ($email && $password) {
@@ -10,9 +14,18 @@ if (isset($_POST['connexion'])) {
 
 		$test = mysqli_query($connect, "SELECT * FROM utilisateur WHERE email = '$email' AND mdp = '$password'");
 
+		$lpseudo = mysqli_query($connect, "SELECT pseudo FROM utilisateur WHERE email ='$email'");
+		$lpseudo = $lpseudo->fetch_array(1);
+
+		$U_ID = mysqli_query($connect, "SELECT U_ID FROM utilisateur WHERE email ='$email'");
+		$U_ID = $U_ID->fetch_array(1);
+
 		if (mysqli_num_rows($test) == 1) {
 		$_SESSION['email'] = $email;
-		echo("Vous êtes connectés "); die($_SESSION['email']);
+		$_SESSION['U_ID'] = $U_ID;
+		$_SESSION['pseudo'] = $lpseudo[pseudo];
+
+		die("Vous êtes connectés $lpseudo[pseudo]");
 		}
 		else die("L'adresse mail ou le mot de passe est incorrect."); 
 
