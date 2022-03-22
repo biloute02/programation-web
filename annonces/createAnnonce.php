739f8035406e4b2c121ab84047dd90b0ecaebc4c
Eau <68661393+Muloot@users.noreply.github.com>
2022-03-22 12:27:14 +0100

    Add files via upload

--- annonces/createAnnonce.php
+++ annonces/createAnnonce.php
@@ -1,8 +1,7 @@
 <?php
-	//if(test isset et pas vide)
-	//{
-		include '../include/connex.inc.php';
-		
+	if(isset($_POST["submit"]) or die("???????????????"))
+	{	
+		include 'connex.php';	
 		$type_l = trim($_POST["logement"]);
 		$date_d = $_POST["date_deb"];
 		$date_f = $_POST["date_fin"];
@@ -13,14 +12,14 @@
 		$prix = trim($_POST["prix"]);
 		$surface = trim($_POST["surface"]);
 		$nb_p = trim($_POST["pieces"]);
+		$photo = trim($_POST["photo"]);
+	}
+	echo $photo;
 		
-		echo $date_d;
-		//...(verif bon format)
-		
-		
+	if(preg_match(('^[0-9]+\ +([a-zA-Z]+|[\-]?)/',"$adrs")	
 		$SQL_INSERT = "INSERT INTO annonce (A_ID, type_logement, date_deb, date_fin, adresse, ville, cp, pays, prix, surface, nb_pieces) VALUES('a', '$type_l', '$date_d', '$date_f', '$adrs', '$ville', $cp, '$pays', '$prix', '$surface', '$nb_p')";		
-		$idcom = connex("myparam") or die(mysqli_error()); //connex est ici !!
+		$idcom = connex("clementmargotin", "login") or die(mysqli_error());
 		mysqli_query($idcom, $SQL_INSERT);
+		//mysqli_query($idcom, "INSERT INTO Photo (path) VALUES ('$photo')");
 		mysqli_close($idcom);
-	//}
 ?>
