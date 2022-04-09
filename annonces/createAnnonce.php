4ec4e803b81169daafe2253d0ac56afd15bc7fe3
Eau <68661393+Muloot@users.noreply.github.com>
2022-04-10 01:27:16 +0200

    prout
    prout²

--- annonces/createAnnonce.php
+++ annonces/createAnnonce.php
@@ -1,27 +1,47 @@
 <?php
-include 'connex.php';
-	if(isset($_POST["submit"])) {		
-		$type_l = $_POST["logement"];
-		$date_d = $_POST["date_deb"];
-		$date_f = $_POST["date_fin"];
-		$adrs = trim($_POST["adresse"]);
-		$ville = trim($_POST["ville"]);
-		$cp = trim($_POST["CP"]);
-		$pays = trim($_POST["pays"]);
-		$prix = trim($_POST["prix"]);
-		$surface = trim($_POST["surface"]);
-		$nb_p = trim($_POST["pieces"]);
-		$photo = trim($_POST["photo"]);
-	
-		echo $photo;
-		
-		if(preg_match('/^[0-9]+\ [a-zA-Z- 0-9]+/', $adrs)) {	
-			$SQL_INSERT = "INSERT INTO annonce (type_logement, date_deb, date_fin, adresse, ville, cp, pays, prix, surface, nb_pieces) VALUES('$type_l', '$date_d', '$date_f', '$adrs', '$ville', $cp, '$pays', $prix, $surface, $nb_p)";
-		
-			$idcom = connex("clementmargotin", "login") or die("Erreur de connexion");
-			mysqli_query($idcom, $SQL_INSERT);
-			$oue = file_put_contents("../image", $photo);
-			mysqli_close($idcom);
+if(isset($_POST["submit"])) {
+	date_default_timezone_set('Europe/Paris');
+	include '../include/connex.inc.php';
+	session_start();
+
+	if (!estConnecte()) {
+		seConnecter();
 		}
+	
+	$U_ID = $_SESSION['U_ID'];
+
+	
+	$type_l = $_POST["logement"];
+	$date_d = $_POST["date_deb"];
+	$date_f = $_POST["date_fin"];
+	$date_p = date('Y-m-d');
+	$adrs = trim($_POST["adresse"]);
+	$ville = trim($_POST["ville"]);
+	$cp = trim($_POST["CP"]);
+	$pays = trim($_POST["pays"]);
+	$desc = trim($_POST["desc"]);
+	$prix = trim($_POST["prix"]);
+	$surface = trim($_POST["surface"]);
+	$nb_p = trim($_POST["pieces"]);
+	$file = $_FILES['photo'];
+	$SQL_INSERT = "INSERT INTO annonce (statut, type_logement, date_deb, date_fin, date_post, adresse, ville, cp, pays, contenu_annonce, prix, surface, nb_pieces, U_ID) VALUES(1, '$type_l', '$date_d', '$date_f', '$date_p', '$adrs', '$ville', $cp, '$pays','$desc', $prix, $surface, $nb_p, $U_ID)";
+	
+	if(preg_match('/^[0-9]+\ [a-zA-Z- 0-9]+/', $adrs)) {	
+		$idcom = connex("myparam") or die("Erreur de connexion");
+		mysqli_query($idcom, $SQL_INSERT);
+		mysqli_close($idcom);
+			
+
+		for ($i = 0; $i < count($file['name']); $i++) {
+			$origine = $file['tmp_name'][$i];
+			$destination = '../photos/'.$file['name'][$i];
+			move_uploaded_file($origine,$destination);
+		}
+		
+	}else{
+		echo "Adresse invalide <a href='./createAnnonce.html'>Retour sur la page de creation</a>";
+		die();
+	}
+	echo "Votre annonce a bien ete poste <a href='../index.php'>Retour sur la page d'accueil</a>";
 	}
 ?>
