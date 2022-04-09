c901da19acc2ab1887c60ea46fc7b1ea58987582
Pandabuffle <100595161+Pandabuffle@users.noreply.github.com>
2022-04-09 11:24:21 +0200

    Update creat_tables.sql

--- creat_tables.sql
+++ creat_tables.sql
@@ -3,60 +3,56 @@ CREATE TABLE Utilisateur (
     email varchar(30) NOT NULL,
     mdp varchar(255) NOT NULL,
     pseudo varchar(15) NOT NULL,
-    nom varchar(50),
+    nom varchar (50),
     prenom varchar(50),
     date_naissance DATE,
-    date_inscription DATETIME,
+    date_inscription DATE,
     PRIMARY KEY(U_ID),
     UNIQUE(email)
 );
 
 CREATE TABLE Photo (
-    P_ID char(15),
-    chemin char(100),
+    P_ID int AUTO_INCREMENT,
+    chemin varchar(100),
     U_ID int NOT NULL,
     FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID),
     PRIMARY KEY(P_ID)
 );
 
 CREATE TABLE Annonce(
-    A_ID INT AUTO_INCREMENT,
-    statut VARCHAR(15),
+    A_ID int AUTO_INCREMENT,
+    statut int,
     type_logement VARCHAR(20),
     date_deb DATE,
     date_fin DATE,
+    date_post DATE,
     adresse VARCHAR(50),
     ville VARCHAR(50),
     cp CHAR(5),
     pays VARCHAR(50),
+    contenu_annonce TEXT,
     prix DECIMAL(6,2),
     surface INT,
     nb_pieces INT,
+    U_ID int NOT NULL,
+    FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID),
     PRIMARY KEY(A_ID)
 );
 
 
 CREATE TABLE reserve(
     U_ID int,
-    A_ID INT,
+    A_ID int,
     statut_res VARCHAR(15),
     PRIMARY KEY(U_ID, A_ID),
     FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID),
     FOREIGN KEY(A_ID) REFERENCES Annonce(A_ID)
 );
 
-CREATE TABLE poste(
-    U_ID int,
-    A_ID INT,
-    date_post DATE,
-    FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID),
-    FOREIGN KEY(A_ID) REFERENCES Annonce(A_ID),
-    PRIMARY KEY(U_ID, A_ID)
-);
-
 CREATE TABLE illustre(
-    P_ID CHAR(15),
-    A_ID INT,
+    P_ID int,
+    A_ID int,
+    num_photo int,
     FOREIGN KEY(P_ID) REFERENCES Photo(P_ID),
     FOREIGN KEY(A_ID) REFERENCES Annonce(A_ID),
     PRIMARY KEY(P_ID, A_ID)
