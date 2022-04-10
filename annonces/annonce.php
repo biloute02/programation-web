edcb9c3597bf2f1a27351a5f2003fcecd6a5f103
NegaSteel <boudoux.lilian@gmail.com>
2022-04-10 18:29:54 +0200

    barre de recherche faite

--- annonces/annonce.php
+++ annonces/annonce.php
@@ -7,11 +7,47 @@
 		$_SESSION['A_ID'] = $_POST['A_ID'];
 		header("Location: reserver.php");
 	}
-
-	//$U_ID = $_SESSION['U_ID'];
+	
 	$idcom = connex("myparam");
-	$query = "SELECT A_ID, statut FROM annonce";
-	//$query = "SELECT A_ID FROM annonce WHERE U_ID != '$U_ID'"
+	$query = "SELECT A_ID, statut FROM annonce WHERE 1";
+	
+	if (!empty($_POST['prixmin'])) {
+		$pmin = $_POST['prixmin'];
+		$query .= " AND prix >= $pmin";
+	}
+	if (!empty($_POST['prixmax'])) {
+		$pmax = $_POST['prixmax'];
+		$query .= " AND prix <= $pmax";
+	}
+	if (!empty($_POST['logement'])) {
+		$log = $_POST['logement'];
+		$query .= " AND type_logement = '$log'";
+	}
+	if (!empty($_POST['datedebut'])) {
+		$dated = $_POST['datedebut'];
+		$query .= " AND date_deb <= STR_TO_DATE('$dated', '%Y-%m-%d') AND STR_TO_DATE('$dated', '%Y-%m-%d') < date_fin";
+	}
+	if (!empty($_POST['datefin'])) {
+		$datef = $_POST['datefin'];
+		$query .= " AND date_fin >= STR_TO_DATE('$datef', '%Y-%m-%d') AND STR_TO_DATE('$datef', '%Y-%m-%d') > date_debut";
+	}
+	if (!empty($_POST['pays'])) {
+		$pays = $_POST['pays'];
+		$query .= " AND UPPER(pays) = UPPER('$pays')";
+	}
+	if (!empty($_POST['ville'])) {
+		$ville = $_POST['ville'];
+		$query .= " AND UPPER(ville) = UPPER('$ville')";
+	}
+	if (!empty($_POST['piecemin'])) {
+		$pmin = $_POST['piecemin'];
+		$query .= " AND nb_pieces >= $pmin";
+	}
+	if (!empty($_POST['piecemax'])) {
+		$pmax = $_POST['piecemax'];
+		$query .= " AND nb_pieces <= $pmax";
+	}
+
 	$result = mysqli_query($idcom, $query);
 	$result = mysqli_fetch_all($result, MYSQLI_BOTH);
 ?>
@@ -29,6 +65,30 @@
 	<a href=../index.php>Share My House</a>
 	</h1>
 	<h2>Recherche Annonce</h2>
+<form method="POST">
+	<label id="pays">Pays :</label>
+	<input type="search" name="pays" id="pays" placeholder="Pays">
+	<br><label id="ville">Ville :</label>
+	<input type="search" name="ville" id="ville" placeholder="Ville">
+	<br><label for="logement_select">Type de logement :</label>
+			<select name="logement" id="logement_select">
+				<option value="" disabled selected>--Choisir une option--</option>
+				<option value="maison">Maison</option>
+				<option value="appart">Appartement</option>
+			</select>
+	<br><label id="prixmin">Prix (€) :</label> 
+	<input type="number" name="prixmin" accesskey="s" placeholder="Minimum" id="prixmin">
+	<input type="number" name="prixmax" accesskey="s" placeholder="Maximum" id="prixmax">
+	<br><label id="datedeb">Dates début | fin:</label>
+	<input type="date" name="datedebut" id="datedeb">
+	<input type="date" name="datefin" id="datefin">
+	<br><label id="piecemin">Nombre de pièces :</label> 
+	<input type="number" name="piecemin" accesskey="s" placeholder="Minimum" id="piecemin">
+	<input type="number" name="piecemax" accesskey="s" placeholder="Maximum" id="piecemax">
+	<br>
+	<input type="submit" name="rechercher" value="Rechercher">
+</form>
+<br>
 <?php
 	if (!empty($result)) {
 		echo '<form method="post">';
@@ -44,5 +104,6 @@
 		echo "</form>";
 	}
 ?>
+
 </body>
 </html>
