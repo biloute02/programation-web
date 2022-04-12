93b6523e4ec1a5ea6dccdba781c472adbc0cf748
NegaSteel <boudoux.lilian@gmail.com>
2022-04-12 16:33:44 +0200

    ajout de saut à la ligne

--- messagerie/envoie.php
+++ messagerie/envoie.php
@@ -13,6 +13,7 @@
 	
 	if (isset($_POST['envoie'])) {
 		$message = htmlentities(trim($_POST['message']), ENT_QUOTES, "UTF-8"); // permet l'ajout des tildes
+		$message = nl2br($message);
 		if ($message) {
 			$query1 = mysqli_query($connect, "INSERT INTO communiquer VALUES('$iddestinataire', '$idutilisateur', now(), '$message')");
 			header("Location:envoie.php");		
